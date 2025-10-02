fef7ef5d70a312e87287a5a72ca4ef1b17368da7
jf <jpf0628@gmail.com>
2025-10-02 10:49:13 -0700

    System preferred color scheme, local storage theme, and toggle color scheme are all respected

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { initTheme } from "./toggleColorScheme.ts";
 import "./index.css";
 import App from "./App.tsx";
-//  initTheme();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
--- src/toggleColorScheme.ts
+++ src/toggleColorScheme.ts
@@ -1,14 +1,20 @@
 /**Self-invoking function checks the saved theme preference in localStorage
  * and the OS-level dark mode setting to apply the correct theme on page load. */
-export default (() => {
+(() => {
   // On page load or when changing themes, best to add inline in `head` to avoid  flash of unstyled content (FOUC)
+  console.log("local theme", localStorage.theme);
+  console.log("theme in localStorage", "theme" in localStorage);
+  console.log(
+    "system theme",
+    window.matchMedia("(prefers-color-scheme:dark)").matches
+  );
   if (
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
+    localStorage.theme === "dark" || (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     document.documentElement.classList.add("dark");
   } else {
+    console.log("remove dark")
     document.documentElement.classList.remove("dark");
   }
 })();
